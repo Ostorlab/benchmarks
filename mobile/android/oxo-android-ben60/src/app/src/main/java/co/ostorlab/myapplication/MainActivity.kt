6bda0ac89fdf6d08b7dc783a1202975abef24e14
amat-osto <amine.atyq@ostorlab.dev>
2025-08-28 13:12:07 +0100

    adding skeleton app

---
+++ mobile/android/oxo-android-ben60/src/app/src/main/java/co/ostorlab/myapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package co.ostorlab.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // No UI yet – completely blank activity
+    }
+}
