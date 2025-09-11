b9bc80c3936c7d424bb9a84644748d5ec5a25cb5
deadly-panda <abderrahim.haddadi@ostorlab.dev>
2025-09-11 10:52:58 +0100

    Lower & minSdk to 26 for Android oxo-ben-56.

--- mobile/android/oxo-android-ben56/src/app/build.gradle.kts
+++ mobile/android/oxo-android-ben56/src/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.securityplus.vault"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.securityplus.vault"
-        minSdk = 32
-        targetSdk = 36
+        minSdk = 26
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
